f49a8eaa6501c6a469078c8f8a7c0e094b515235
archie <archieZeki@gmail.com>
2025-04-11 23:09:55 +0100

    feat: itll get better

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,22 @@ function App() {
 
     return (
         <>
+            <div className="word">
+                <div className="home">
 
-            <div className="home">
+                    <div>
+                        <h1>Archie Zeki</h1>
+                        <div className="">
+                            <p className="text">
+                                something something kotlin java something
+                            </p>
+                        </div>
 
-                <div>
-                    <h1>Archie Zeki</h1>
-                    <div className="">
-                        <p className="text">
-                            something something kotlin java something
-                        </p>
                     </div>
-
                 </div>
 
 
-                <div className="home flex justify-between">
+                <div className="home flex justify-between pic">
                     <a href="/src/assets/duck.jpg">
                         <img src={duck} className="logo" alt="Me"/>
                     </a>
