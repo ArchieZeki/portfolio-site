c1d00962925cd47a2323698ceb4468987b8a268f
archie <archieZeki@gmail.com>
2025-04-13 22:14:50 +0100

    feat: a better layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,24 @@ function App() {
 
     return (
         <>
-            <div className="home">
-                <div className="name">
-                    <h1>Archie Zeki</h1>
-                    <div className="flex flex-col gap-8">
-                              <span
-                                  className="title text-[90px] font-extrabold leading-none tracking-wide text-primary-light dark:text-primary-dark whitespace-pre-line">
-                                Java/Kotlin Developer
-                              </span>
-                    </div>
+            <div className="main-container">
+                <div className="space">
                 </div>
-                <div className="pic">
-                    <a href="/src/assets/duckMe.png">
-                        <img src={duck} className="me" alt="Me"/>
-                    </a>
+                <div className="home">
+                    <div className="name">
+                        <h1>Archie Zeki</h1>
+                        <div className="flex flex-col gap-8">
+                                  <span
+                                      className="title text-[90px] font-extrabold leading-none tracking-wide text-primary-light dark:text-primary-dark whitespace-pre-line">
+                                    Java/Kotlin Developer
+                                  </span>
+                        </div>
+                    </div>
+                    <div className="pic">
+                        <a href="/src/assets/duckMe.png">
+                            <img src={duck} className="me" alt="Me"/>
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
